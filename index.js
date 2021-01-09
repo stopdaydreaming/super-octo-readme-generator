bef1d8420ab62b82da1a6b30083f0945c6620cbb
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-08 23:06:18 -0500

    cleaned up functions in index file

--- index.js
+++ index.js
@@ -1,93 +1,104 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        message: "Project Title:",
-        name: "title"
-    },
-    {
-        type: "input",
-        message: "Project Description:",
-        name: "description"
-    },
-    {
-        type: "editor",
-        message: "Table of Contents:",
-        name: "contents"
-    },
-    {
-        type: "input",
-        message: "Installation Instructions:",
-        name: "installation"
-    },
-    {
-        type: "input",
-        message: "Project Usage:",
-        name: "usage"
-    },
-    {
-        type: "list",
-        choices: [
-            "None",
-            "Apache License 2.0",
-            "GNU General Public License v3.0",
-            "MIT License",
-            "BSD 2-Clause Simplified License",
-            "BSD 3-Clause New or Revised License",
-            "Boost Software License 1.0",
-            "Creative Commons Zero v1.0 Universal",
-            "Eclipse Public License 2.0",
-            "GNU Affero General Public License v3.0",
-            "GNU General Public License v2.0",
-            "GNU Lesser General Public License v2.1"
-        ],
-        message: "Choose a License:",
-        name: "license"
-    },
-    {
-        type: "list",
-        choices: [
-            "Please contact the author to be added as a collaborator to this project and to discuss contribution guidelines.",
-            "No contributions are being accepted at this time."
-        ],
-        message: "Contributing:",
-        name: "contributing"
-    },
-    {
-        type: "input",
-        message: "Testing:",
-        name: "testing"
-    },
-    {
-        type: "input",
-        message: "Questions:",
-        name: "questions"
-    },
-    {
-        type: "input",
-        message: "Reporting Issues:",
-        name: "issues"
-    }
+  {
+    type: "input",
+    message: "Project Title:",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Project Description:",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Table of Contents:",
+    name: "contents"
+  },
+  {
+    type: "input",
+    message: "Installation Instructions:",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message: "Project Usage:",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Collaborator Credits:",
+    name: "credits"
+  },
+  {
+    type: "list",
+    choices: [
+      "None",
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+      "BSD 2-Clause Simplified License",
+      "BSD 3-Clause New or Revised License",
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU Lesser General Public License v2.1"
+    ],
+    message: "Choose a License:",
+    name: "license"
+  },
+  {
+    type: "list",
+    choices: [
+      "Please contact the author to be added as a collaborator to this project and to discuss contribution guidelines.",
+      "No contributions are being accepted at this time."
+    ],
+    message: "Contributing:",
+    name: "contributing"
+  },
+  {
+    type: "input",
+    message: "Testing:",
+    name: "testing"
+  },
+  {
+    type: "input",
+    message: "Questions:",
+    name: "questions"
+  },
+  {
+    type: "input",
+    message: "Reporting Issues:",
+    name: "issues"
+  }
 ];
 
-const promptUser = () => {
-    inquirer.prompt([...questions]).then((response) => {
-        console.log("success");
-    })
-}
-
 // TODO: Create a function to write README file
-const writeToFile = (fileName, data) => {}
+const writeToFile = (file, data) => {
+  const dynamicString = generateMarkdown(data);
+  fs.writeFile(file, dynamicString, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("README file generated");
+    }
+  });
+};
 
 // TODO: Create a function to initialize app
 const init = () => {
-    console.log("function intialized");
-}
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+    writeToFile("GENERATED_README.MD", response);
+  });
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = (license) => {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (license) => {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = (data) => {
+  `
+  # ${data.title}
 
-`;
+  ## Description 
+  
+  ${data.description}
+  
+  ## Table of Contents (Optional)
+  
+  ${data.contents}
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  
+  ## Usage 
+  
+  ${data.usage}
+  ![alt text](assets/images/screenshot.png)
+  
+  ## Credits
+  
+  ${data.credits}
+  
+  ## License
+
+  ${data.license}
+  
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.testing}
+
+  ## Questions
+  
+  ${data.testing}
+  
+  ## Issues
+  
+  ${data.testing}
+  `;
 }
 
 module.exports = generateMarkdown;
