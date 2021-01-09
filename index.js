f74db6283dde283bfde30d6567273a1e74e907ea
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-08 20:17:36 -0500

    added readme template, readme questions

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+// 1)project title 
+// 2)project description 
+// 3)table of contents
+// 4)installation
+// 5)usage
+// 6)license
+// 7)contributing
+// 8)tests
+// 9)questions
+// 10)how to report issues 
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
